817965a82c026fc265bc4ea3752180d0545736f7
m.falahatnejad <m.falahatnejad@tosantechno.net>
2023-04-30 15:11:15 +0330

    validate bill payment

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ billID = 9229799804123;
 let paymentId = paymentID.toString().split("").map(Number);
 let billId = billID.toString().split("").map(Number);
 let paymentIdArray = paymentId.slice(0, paymentId.length - 2);
-let bothId = paymentId.slice(0, paymentId.length - 1).concat(paymentId);
+let bothIdArray = billId.concat(paymentId.slice(0, paymentId.length - 1));
 let firstCheckDigit = Number(paymentId.slice(paymentId.length - 2, paymentId.length - 1));
 let secondCheckDigit = Number(paymentId.slice(paymentId.length - 1, paymentId.length));
 
@@ -27,4 +27,5 @@ const BaseElevenFormula = (billArray) => {
   return finalFirstcheckDigit;
 };
 
-BaseElevenFormula(paymentIdArray);
+console.log(BaseElevenFormula(paymentIdArray)); // for first check digit => payment id => 5
+console.log(BaseElevenFormula(bothIdArray)); // for second check digit => payment and bill id => 4
